ddfc9fbfc876fc7cbd423b3c06cdbd412d60f09f
Fabio Niephaus <code@fniephaus.com>
2015-05-10 20:49:35 +0200

    Use chromium-browser

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['chromium-browser'],
 
     browserNoActivityTimeout: 60000,
 
