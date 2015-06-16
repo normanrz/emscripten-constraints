293364fbe5ca01104058ee183eb132046ccae0d3
Philipp Otto <philipp@scm.io>
2015-06-16 16:37:08 +0200

    clean up after environment polluting emscripten

--- z3/loader.js
+++ z3/loader.js
@@ -2,7 +2,7 @@ var memoryInitializerPath = null;
 function loadModule(moduleUrl, path, callback) {
     var solverObj = null;
     memoryInitializerPath = path;
-    
+
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         var DONE = request.DONE || 4;
@@ -11,11 +11,14 @@ function loadModule(moduleUrl, path, callback) {
                 debugger
                 console.log("Evaluating asmjs code...");
                 solverObj = new Function(request.responseText)();
+                // emscripten puts Module into global namespace if it
+                // determines that it runs in the web
+                delete window.Module;
                 callback(solverObj);
             } else {
                 console.error("Error while loading ", moduleUrl);
             }
-        } 
+        }
     };
     request.open("GET", path + moduleUrl);
     request.send();
