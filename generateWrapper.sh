d7e98df9ae21a4cee7df8b23a489b2c86945eefb
Philipp Otto <philipp@scm.io>
2015-07-14 11:45:01 +0200

    add amd option to generateWrappers (#10)

--- generateWrapper.sh
+++ generateWrapper.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Concatenating files..."
 cp $1 $2
-if [[ $1 = "common" ]]; then
+if [[ $3 = "common" ]]; then
   echo "exports.Module = Module; exports.FS = FS; " >> $2
+elif [[ $3 = "amd" ]]; then
+  sed -i -e "1s/^/define([], function() {\n/" $2
+  echo "return { Module : Module, FS : FS };};" >> $2
 else
   echo "return { Module : Module, FS : FS };" >> $2
 fi
